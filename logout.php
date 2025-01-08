8fa2ada84de6fa9e2baf7d82f0828770404eaa3b
arrel123 <arremilk13@gmail.com>
2025-01-08 15:17:06 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -4,6 +4,20 @@
 // Dibuat Oleh : Rafi Akhbar Dirgahayuri (NIM: 3312401065)
 // Tanggal Dibuat : 20 Desember 2024
 
+/*
+Pseudocode:
+
+1. Mulai
+2. Inisialisasi sesi:
+    - Panggil session_start() untuk memulai sesi dan memastikan akses ke sesi yang benar
+3. Hapus Data Session:
+    - Panggil session_unset() untuk menghapus semua variabel session yang ada
+    - Panggil session_destroy() untuk menghancurkan sesi secara keseluruhan
+4. Redirect ke Halaman Awal:
+    - Arahkan pengguna ke halaman utama (resteasy.php)
+5. Akhiri
+*/
+
 session_start(); // Memulai sesi untuk memastikan kita mengakses sesi yang benar
 session_unset(); // Menghapus semua variabel session yang tersimpan
 session_destroy(); // Menghancurkan sesi secara keseluruhan
